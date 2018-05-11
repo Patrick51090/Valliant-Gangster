9a3817664079e8e0314c65003a981d413d50a969
Patrick51090 <patrick.carroll715@gmail.com>
2018-05-10 21:41:16 -0400

    Updated Foostep Audio

--- assets/scenes/MainWorld.js
+++ assets/scenes/MainWorld.js
@@ -24,7 +24,7 @@ class MainWorldScene extends Phaser.Scene {
         this.map_tiles = this.map.addTilesetImage('ground_tiles', 'img-ground_tiles');
         this.map_layer1 = this.map.createStaticLayer('Ground', this.map_tiles, 0, 0);
 
-        
+
 
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
@@ -57,10 +57,14 @@ class MainWorldScene extends Phaser.Scene {
         this.map_layer1.setTileIndexCallback(20, FootstepAudio.Sand, this);
 
 
-        //Stone tiles 88, 107, 91
+        //Stone tiles 88, 107, 91, 119, 140, 118, 89
         this.map_layer1.setTileIndexCallback(88, FootstepAudio.Stone, this);
         this.map_layer1.setTileIndexCallback(107, FootstepAudio.Stone, this);
         this.map_layer1.setTileIndexCallback(91, FootstepAudio.Stone, this);
+        this.map_layer1.setTileIndexCallback(119, FootstepAudio.Stone, this);
+        this.map_layer1.setTileIndexCallback(140, FootstepAudio.Stone, this);
+        this.map_layer1.setTileIndexCallback(118, FootstepAudio.Stone, this);
+        this.map_layer1.setTileIndexCallback(89, FootstepAudio.Stone, this);
 
 
 
